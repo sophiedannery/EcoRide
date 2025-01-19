d61d51034188cfd34fa0afdd5c56b1dd3853fba4
sophiedannery <dannery.sophie@gmail.com>
2025-01-19 19:59:26 +0100

    Formulaire d'inscription & bdd

--- inscription.php
+++ inscription.php
@@ -1,29 +1,73 @@
 <?php
 require_once 'templates/header.php';
+require_once 'lib/pdo.php';
+require_once 'lib/utilisateur.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $verif = verifyUSer($pdo, $_POST);
+
+    if ($verif === true) {
+        $resAdd = addUSer($pdo, $_POST["pseudo"], $_POST["email"], $_POST["mot_de_passe"]);
+        header("Location: index.php");
+    } else {
+        $errors = $verif;
+    }
+}
 ?>
+
+
 
 <div class="container">
     <h1>Inscription</h1>
 
     <form method="post">
+
+        <!-- pseudo -->
         <div class="mb-3">
             <label for="pseudo" class="form-label">Pseudo</label>
-            <input type="text" class="form-control" id="pseudo">
-        </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">E-mail</label>
-            <input type="email" class="form-control" id="email">
-        </div>
-        <div class="mb-3">
-            <label for="motDePasse" class="form-label">Mot de passe</label>
-            <input type="motDePasse" class="form-control" id="motDePasse">
-        </div>
-        <div class="mb-3">
-            <label for="confirmeMotDePasse" class="form-label">Confirmer le mot de passe</label>
-            <input type="confirmeMotDePasse" class="form-control" id="confirmeMotDePasse">
+            <input type="text" class="form-control" id="pseudo" name="pseudo" value="<?= isset($_POST['pseudo']) ? htmlspecialchars($_POST['pseudo']) : '' ?>">
+            <?php if (isset($errors["pseudo"])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= $errors["pseudo"] ?>
+                </div>
+            <?php } ?>
         </div>
 
-        <button type="submit" class="btn btn-primary w-100">S'inscrire</button>
+        <!-- email -->
+        <div class="mb-3">
+            <label for="email" class="form-label">E-mail</label>
+            <input type="email" class="form-control" id="email" name="email" value="<?= isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '' ?>">
+            <?php if (isset($errors["email"])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= $errors["email"] ?>
+                </div>
+            <?php } ?>
+        </div>
+
+        <!-- mot de passe -->
+        <div class="mb-3">
+            <label for="mot_de_passe" class="form-label">Mot de passe</label>
+            <input type="password" class="form-control" id="mot_de_passe" name="mot_de_passe">
+            <?php if (isset($errors["mot_de_passe"])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= $errors["mot_de_passe"] ?>
+                </div>
+            <?php } ?>
+        </div>
+
+        <!-- confirmation mot de passe -->
+        <div class="mb-3">
+            <label for="confirm_mot_de_passe" class="form-label">Confirmer le mot de passe</label>
+            <input type="password" class="form-control" id="confirm_mot_de_passe" name="confirm_mot_de_passe">
+            <?php if (isset($errors["confirm_mot_de_passe"])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= $errors["confirm_mot_de_passe"] ?>
+                </div>
+            <?php } ?>
+        </div>
+
+        <!-- bouton -->
+        <button type="submit" class="btn btn-primary w-100" name="add_user">S'inscrire</button>
     </form>
 </div>
 
---
+++ lib/utilisateur.php
@@ -0,0 +1,70 @@
+<?php
+
+function addUser(PDO $pdo, string $pseudo, string $email, string $mot_de_passe): bool
+{
+    $query = $pdo->prepare("INSERT INTO utilisateurs (pseudo, email, mot_de_passe) VALUES (:pseudo, :email, :mot_de_passe)");
+
+    $mot_de_passe = password_hash($mot_de_passe, PASSWORD_DEFAULT);
+
+    $query->bindValue(':pseudo', $pseudo);
+    $query->bindValue(':email', $email);
+    $query->bindValue(':mot_de_passe', $mot_de_passe);
+
+    return $query->execute();
+}
+
+
+
+function verifyUSer(PDO $pdo, $utilisateur): array | bool
+{
+    $errors = [];
+
+    if (isset($utilisateur["pseudo"])) {
+        if ($utilisateur["pseudo"] === "") {
+            $errors["pseudo"] = "Le champ pseudo est obligatoire";
+        }
+    } else {
+        $errors["pseudo"] = "Le champ pseudo n'a pas été envoyé";
+    }
+
+    if (isset($utilisateur["mot_de_passe"])) {
+        if (strlen($utilisateur["mot_de_passe"]) < 8) {
+            $errors["mot_de_passe"] = "Le mot de passe doit faire au moins 8 caractères";
+        }
+    } else {
+        $errors["mot_de_passe"] = "Le champ mot_de_passe n'a pas été envoyé";
+    }
+
+    if (isset($utilisateur["mot_de_passe"]) && isset($utilisateur["confirm_mot_de_passe"])) {
+        if ($utilisateur["mot_de_passe"] != $utilisateur["confirm_mot_de_passe"]) {
+            $errors["confirm_mot_de_passe"] = "Les mots de passe ne correspondent pas";
+        }
+    }
+
+    if (isset($utilisateur["email"])) {
+        if ($utilisateur["email"] === "") {
+            $errors["email"] = "Le champ email est obligatoire";
+        } else {
+            if (!filter_var($utilisateur["email"], FILTER_VALIDATE_EMAIL)) {
+                $errors["email"] = "Le format d'email n'est pas respecté";
+            } else {
+                $stmt = $pdo->prepare("SELECT COUNT(*) FROM utilisateurs WHERE email = :email");
+                $stmt->bindValue(":email", $utilisateur["email"]);
+                $stmt->execute();
+                $count = $stmt->fetchColumn();
+
+                if ($count > 0) {
+                    $errors["email"] = "Cet email est déjà utilisé.";
+                }
+            }
+        }
+    } else {
+        $errors["email"] = "Le champ email n'a pas été envoyé";
+    }
+
+    if (count($errors)) {
+        return $errors;
+    } else {
+        return true;
+    }
+}
