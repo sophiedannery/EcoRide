4cf8ebbdc8248e7946ced6d884b686bf4ab561e7
sophiedannery <dannery.sophie@gmail.com>
2025-01-19 16:11:48 +0100

    Installation SASS

--- index.php
+++ index.php
@@ -6,12 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EcoRide</title>
 
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <!-- appel de SASS -->
+    <link rel="stylesheet" href="scss/main.css">
 </head>
 
 <body>
+    <header>
 
+    </header>
+
+    <main>
+
+    </main>
+
+    <h1>Hello World</h1>
+
+    <footer>
+
+    </footer>
+    <!-- appel JS de Bootstrap -->
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
